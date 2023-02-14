aea61ca4b84b6a4c6bc306c586d3714b486499fb
tadcos29 <taddeo.costanzo@alumni.utoronto.ca>
2023-02-14 03:50:32 -0500

    Add colour check and dk-grey/lt-grey defaults. Tweak font height.

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
 const inq = require('inquirer');
-// const MLIP = require('inquirer-maxlength-input-prompt');
-// inq.registerPrompt('userCriteria', MLIP)
+ const MLIP = require('inquirer-maxlength-input-prompt');
+ const csscolors = require('css-color-names');
+ inq.registerPrompt('maxLengthPrompt', MLIP)
 const fs = require('fs');
-const {Shape, Circle, Triangle, Square} = require('./lib/shapes.js')
-
+const {Shape, Circle, Triangle, Square} = require('./lib/shapes.js');
 let hexTest=  /^#[0-9a-f]{3,6}$/i
+const ColourCheck = (str) => hexTest.test(str) || csscolors.hasOwnProperty(str.toLowerCase());
+
 
 const userCriteria = [
-    {type:'input', message: 'Please input up to three characters of text:', name:'sText'},
+    {type:'maxLengthPrompt', message: 'Please input up to three characters of text:', name:'sText', maxLength:3},
     {type:'input', message: `Please enter the text colour:`, name:'sTextColour'},
     {type:'list', message: 'Please select a shape:',choices:['Circle','Triangle','Square'], name:'sShape'},
     {type:'input', message: 'Please enter the shape colour:', name:'sShapeColour'},
 ];
 
-function init() {
 
+
+function init() {
+    console.log(ColourCheck('sarah'));
     let userInput = inq.prompt(userCriteria)
     .then((userInput) => {console.log(userInput);
-    generateSVG(userInput.sText, userInput.sTextColour, userInput.sShape, userInput.sShapeColour);
-    });
+        if (!ColourCheck(userInput.sTextColour)) {console.log(`Invalid text colour: ${userInput.sTextColour}. Defaulting to dark grey.`); userInput.sTextColour='darkgray'}
+        if (!ColourCheck(userInput.sShapeColour)) {console.log(`Invalid shape colour: ${userInput.sShapeColour}. Defaulting to dark grey.`); userInput.sShapeColour='lightgray'}
+        generateSVG(userInput.sText, userInput.sTextColour, userInput.sShape, userInput.sShapeColour);
+        });
 
 }
 
 function generateSVG(gsText, gsTextColour, gsShape, gsShapeColour) {
-    console.log('inside'+gsText);
     let theShape = eval(`new ${gsShape} (gsText,gsTextColour,gsShape,gsShapeColour);`);
     console.log('circle is first '+theShape);
     console.log('circle is '+theShape.render());
